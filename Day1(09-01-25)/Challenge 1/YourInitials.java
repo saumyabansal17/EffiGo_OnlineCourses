deff5e4e10d696156014cedcd7fad48c378dcd50
saumyabansal17 <92037748+saumyabansal17@users.noreply.github.com>
2025-01-10 01:06:35 +0530

    Add files via upload

---
+++ Day1(09-01-25)/Challenge 1/YourInitials.java
@@ -0,0 +1,11 @@
+public class YourInitials {
+    public static void main(String[] args) {
+        System.out.println(" SSS   BBBB ");
+        System.out.println("S   S  B   B");
+        System.out.println("S      B   B");
+        System.out.println(" SSS   BBBB ");
+        System.out.println("    S  B   B");
+        System.out.println("S   S  B   B");
+        System.out.println(" SSS   BBBB ");
+    }
+}
